51d254ba28212d676662e529bfac7333618fc442
Olasubomi Efuniyi <subsefun@gmail.com>
2021-01-20 13:48:50 -0500

    started working on creating game world

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ const WAITING = 3;
 const PLAYING = 4;
 const FINISHED = 5;
 
+let ws = null;
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -22,20 +24,44 @@ class App extends React.Component {
       //4. WAITING - A user is waiting for other players to join a created game
       //5. PLAYING - A user is playing a game
       //6. FINISHED - A user has finished playing a game
-      gameState: [WELCOMING]
+      gameState: [WELCOMING], //game states will be pushed into this array to allow a user to go back if they want to
+      gameCode: null
     }
   }
+
   componentDidMount() {
-    const ws = new WebSocket("ws://localhost:8080");
+    ws = new WebSocket("ws://localhost:8080"); //This web socket is used to comminicate with the server to setup a game
 
     ws.addEventListener('error', (err) => {
       console.log(`Error occured while creating web socket: ${err}`);
     });
+
+    ws.addEventListener('message', (message) => {
+      let msg = JSON.parse(message.data);
+      console.log(msg);
+      this.handleMessageFromServer(msg);
+    });
+  }
+
+  handleMessageFromServer(msg) {
+    if(msg.status === "SUCCESS") {
+      switch(msg.type) {
+        case "CREATE":
+          this.state.gameState.push(CREATING);
+          this.setState({gameCode: msg.gameCode, gameState: this.state.gameState});
+          break;
+        default:
+          break;
+      }
+    }
   }
 
   handleCreateGame() {
-    this.state.gameState.push(CREATING);
-    this.setState({gameState: this.state.gameState});
+    //Tell the server to create a new game world
+    let command = {
+      type: "CREATE"
+    };
+    ws.send(JSON.stringify(command));
   } 
 
   handleJoinGame() {
@@ -66,7 +92,8 @@ class App extends React.Component {
       case CREATING:
         return (
           <div id ="app-container">
-            <Create 
+            <Create
+            gameCode={this.state.gameCode} 
             handleJoinGame={() => this.handleJoinGame()}
             handleBack={() => this.handleBack()} />
           </div>
--- src/components/create.js
+++ src/components/create.js
@@ -5,7 +5,7 @@ class Create extends React.Component {
     render() {
         return (
             <div id="create-container">
-                <p>Your game code is ...</p>
+                <p>Your game code is {this.props.gameCode}</p>
                 <p>Copy and share the game code with other players</p>
 
                 <button onClick={(e) => this.props.handleJoinGame()}>Join Game</button>
